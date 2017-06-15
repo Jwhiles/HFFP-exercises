9fdd0d8ccaff359b353b38699d9c8d5e4a372fd2
John Whiles <john.whiles@justgiving.com>
2017-06-15 15:29:17 +0100

    start working on ch7 exercises

---
+++ exercises/7chapterexercises.hs
@@ -0,0 +1,39 @@
+-- 1
+-- D
+
+-- 2
+f :: Char -> String
+f = undefined
+g :: String -> [String]
+g = undefined
+
+h = g . f
+-- has type signature of Char -> [String]
+-- 3
+three :: Ord a => a -> a -> Bool
+three = undefined
+-- applying Num a to three gives the type Num a -> Bool
+
+-- 4
+-- a function with the type (a -> b) -> c is a higher order function
+
+-- 5 Applying a value to id returns the value. so f True :: Bool
+
+-- Let's write code
+-- 1
+tensDigit :: Integral a => a -> a
+tensDigit x = d 
+  where (xLast, _) = divMod x 10
+        (_, d)     = divMod xLast 10
+
+hunsDigit :: Integral a => a -> a
+hunsDigit x = d2
+  where (xLast, _) = divMod x 100
+        (_, d2)     = divMod xLast 10
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y z = case z of
+  True  -> x
+  False -> y
+--foldBool' :: a -> a -> Bool -> a
+
