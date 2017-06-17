56fe3c55485b8e72d4112efd0cf8b7ea26fb9dc2
John Whiles <johnwhiles@outlook.com>
2017-06-17 11:53:33 +0100

    add more chapter 7 stuff

--- exercises/7.hs
+++ exercises/7.hs
@@ -15,3 +15,29 @@ addOneIfOdd n = case odd n of
 addFive = \x -> \y -> (if x > y then y else x) + 5
 
 mflip x y f = f y x
+
+-- Guard duty
+avgGrade :: (Fractional a, Ord a) => a -> Char
+avgGrade x
+  | y >= 0.9 = 'a'
+  | y >= 0.8 = 'b'
+  | y >= 0.7 = 'c'
+  | y >= 0.6 = 'd'
+  | y <  0.6 = 'f'
+  where y = x / 100
+-- guard blocks evaluate top to bottom, returning
+-- as soon as a condition evaluates to true
+
+-- pal returns True for palindromes
+pal :: Eq a => [a] -> Bool
+pal xs
+  | xs == reverse xs = True
+  | otherwise        = False
+
+-- numbers returns an indication of whether 
+-- the argument is positive or negative
+numbers :: (Ord a, Num a) => a -> a
+numbers x
+  | x < 0  = -1
+  | x == 0 = 0
+  | x > 0  = 1
