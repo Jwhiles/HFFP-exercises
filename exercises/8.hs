9a370e64dbe3baf25521aaf7fc62ced677b52413
John Whiles <johnwhiles@outlook.com>
2017-06-22 07:49:35 +0100

    add wordNumber solution

--- exercises/8.hs
+++ exercises/8.hs
@@ -1,21 +1,21 @@
 -- recursion 1
-sumToN :: Integer -> Integer
-sumToN num = go num 0 0
- where go n i c 
-        | i > n     = (c)
-        | otherwise = go n (i + 1) (c + i)
-
-intMult :: (Integral a) => a -> a -> a
-intMult x y = go x (y - 1) x 
- where go a b prod
-        | b == 0 = prod
-        | otherwise = go a (b - 1) (prod + a)
-
-mc91 :: (Num a, Ord a) => a -> a
-mc91 n
- | n > 100   = n - 10
- | otherwise = mc91 . mc91 $ n + 11
-
+-- sumToN :: Integer -> Integer
+-- sumToN num = go num 0 0
+--  where go n i c 
+--         | i > n     = (c)
+--         | otherwise = go n (i + 1) (c + i)
+-- 
+-- intMult :: (Integral a) => a -> a -> a
+-- intMult x y = go x (y - 1) x 
+--  where go a b prod
+--         | b == 0 = prod
+--         | otherwise = go a (b - 1) (prod + a)
+-- 
+-- mc91 :: (Num a, Ord a) => a -> a
+-- mc91 n
+--  | n > 100   = n - 10
+--  | otherwise = mc91 . mc91 $ n + 11
+-- 
 -- numbers into words
 module WordNumber where
 import Data.List (intersperse)
@@ -31,9 +31,13 @@ digitToWord 6 = "six"
 digitToWord 7 = "seven"
 digitToWord 8 = "eight"
 digitToWord 9 = "nine"
+digitToWord _ = "more than one digit"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits i = reverse (go i)
+  where go d
+         | d < 1 = []
+         | otherwise = d `mod` 10 : go (d `div` 10)
 
-wordNumber :: Int -> String
-wordNumber n = undefined
+--wordNumber :: Int -> String
+wordNumber n = concat $ intersperse "-" (map digitToWord $ digits n)
