8ff02270aad0d40e5f349559ddefee14ae932084
John Whiles <johnwhiles@outlook.com>
2017-06-22 07:24:23 +0100

    add mc91 solution

--- exercises/8.hs
+++ exercises/8.hs
@@ -10,3 +10,30 @@ intMult x y = go x (y - 1) x
  where go a b prod
         | b == 0 = prod
         | otherwise = go a (b - 1) (prod + a)
+
+mc91 :: (Num a, Ord a) => a -> a
+mc91 n
+ | n > 100   = n - 10
+ | otherwise = mc91 . mc91 $ n + 11
+
+-- numbers into words
+module WordNumber where
+import Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord 0 = "zero"
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
+
+digits :: Int -> [Int]
+digits n = undefined
+
+wordNumber :: Int -> String
+wordNumber n = undefined
