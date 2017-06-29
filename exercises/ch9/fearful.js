8d620a0479b70e260a1948c75b5704b058d139fe
John Whiles <johnwhiles@outlook.com>
2017-06-29 21:42:14 +0100

    finish fearful symettry exercises

--- exercises/ch9/fearful.js
+++ exercises/ch9/fearful.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-safeTail :: [a] -> Maybe [a]
-safeTail []      = Nothing
-safeTail (x:[])  = Nothing
-safeTail (_:xs)  = Just xs
-=======
 -- Enum doesn't imply an instance of Ord 
 -- but we can use fromEnum to force the same behaviour
 -- ??
@@ -43,4 +37,38 @@ myWords2 words = go words []
             where
               currentWord = getWord w;
               newWords = removeWord ((length currentWord) + 1) w 
->>>>>>> bc13122cc222539795a972c559c739910577b38e
+
+myWords3 :: String -> [String]
+myWords3 [] = []
+myWords3 (' ':rest) = myWords3 rest
+myWords3 s = w: myWords3 t
+  where
+    w = takeWhile(/= ' ') s
+    t = dropWhile(/= ' ') s
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful symmetry?"
+sentences = firstSen ++ secondSen
+          ++ thirdSen ++ fourthSen
+
+myLines :: String -> [String]
+myLines [] = [] 
+myLines ('\n':s) = myLines s
+myLines s = w: myLines t
+  where
+    w = takeWhile(/= '\n') s
+    t = dropWhile(/= '\n') s
+
+splitAtChar :: Char -> String -> [String]
+splitAtChar c s = go s
+  where
+    go str
+     | str == [] = []
+     | head str == c = go (tail str)
+     | otherwise = w : go t
+                  where 
+                    w = takeWhile(/= c) str
+                    t = dropWhile(/= c) str
+
