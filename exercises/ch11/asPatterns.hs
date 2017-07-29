184b9f12fcadd9d929c0f60513f5265fddf412cc
John Whiles <johnwhiles@outlook.com>
2017-07-29 12:53:25 +0100

    asPattern exercises

--- exercises/ch11/asPatterns.hs
+++ exercises/ch11/asPatterns.hs
@@ -1,4 +1,19 @@
-fibo :: [Integer]
-fibo = go [2,1]
-  -- where go xs@(x:y:_) = go ((x+y):xs)
-  where go (x:y:xs) = go ((x+y):x:y:xs)
+import Data.List (words)
+import Data.Char (toUpper)
+
+doubleUp :: [a] -> [a]
+doubleUp [] = []
+doubleUp xs@(x:_) = x:xs
+
+isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
+isSubsequenceOf [] _ = True
+isSubsequenceOf _ [] = False 
+isSubsequenceOf s@(a:as) (b:bs) =
+  (a == b && isSubsequenceOf as bs || isSubsequenceOf s bs)
+
+capitaliseWords :: String -> [(String, String)]
+capitaliseWords = map (capWord) . words 
+
+capWord :: String -> (String, String)
+capWord [] = ([], [])
+capWord s@(x:xs) = (s, toUpper x:xs)
