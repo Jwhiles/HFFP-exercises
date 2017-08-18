784fc639983226bde5ea3c13d2acc66a76db3ad3
John Whiles <john.whiles@justgiving.com>
2017-08-18 17:00:35 +0100

    add more maybe solutions

--- exercises/ch12/maybe.hs
+++ exercises/ch12/maybe.hs
@@ -5,3 +5,25 @@ isJust _        = False
 isNothing :: Maybe a -> Bool
 isNothing Nothing = True
 isNothing _       = False
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee _ f (Just x)  = f x
+mayybee ac _ Nothing  = ac
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe _ (Just x) = x
+fromMaybe x Nothing  = x
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:_) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes = concat . map maybeToList
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe = undefined
