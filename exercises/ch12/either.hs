076587c1c41a5766951b53bd5894275a89268411
John Whiles <john.whiles@justgiving.com>
2017-08-21 10:51:58 +0100

    add some eithjer solutions

--- exercises/ch12/either.hs
+++ exercises/ch12/either.hs
@@ -10,4 +10,18 @@ rights' = foldr
                                (Right b) -> b:acc)
           []
 
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' = foldr
+                     (\x (as, bs) -> case x of (Left a)  -> (a:as, bs)
+                                               (Right b) -> (as, b:bs))
+                                               ([], []) 
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' _ (Left a) = Nothing
+eitherMaybe' f (Right b) = Just (f b)
 
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' f _ (Left a) = f a
+either' _ f (Right b) = f b
+
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' f xs = either' (\_ -> Nothing) (\x -> Just (f x)) xs
