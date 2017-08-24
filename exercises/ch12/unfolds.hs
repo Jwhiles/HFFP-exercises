f2e9b539d4a1ebf5c195e5fdd91f175004d628f2
John Whiles <johnwhiles@outlook.com>
2017-08-24 17:52:27 +0100

    finish unfoldr exercises

--- exercises/ch12/unfolds.hs
+++ exercises/ch12/unfolds.hs
@@ -3,4 +3,11 @@ myIterate f x = x:(myIterate f (f x))
 
 myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
 -- need to also deal with the maybeness
-myUnfoldr f x = (fst $ f x):(myUnfoldr f (snd $ f x))
+myUnfoldr f x =
+  case (f x) of
+    (Just (a, b)) -> a:(myUnfoldr f b)
+    Nothing -> []
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f = myUnfoldr (\b -> Just (b, f b))
+
