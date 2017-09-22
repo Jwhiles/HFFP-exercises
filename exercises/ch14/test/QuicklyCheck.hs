fd006db3fbbc42e688e1fdb3bb9c2964ebe70a52
John Whiles <john.whiles@justgiving.com>
2017-09-22 14:26:05 +0100

    add test for idempotence of capitalisation

--- exercises/ch14/test/QuicklyCheck.hs
+++ exercises/ch14/test/QuicklyCheck.hs
@@ -2,7 +2,7 @@ module QuicklyCheck where
 
 import Test.QuickCheck
 import Test.Hspec
-
+import Data.Char (toUpper)
 half x = x / 2
 
 halfIdentity = (*2) . half
@@ -13,3 +13,16 @@ propertyTest = hspec $ do
   describe "a property test" $ do
     it "half x * 2 == x" $ do
       property $ \x -> halfIdentity x == (x :: Float) 
+
+twice f = f . f
+fourTimes = twice . twice
+
+capitalizeWord :: String -> String
+capitalizeWord [] = []
+capitalizeWord s = (toUpper $ head s):(tail s)
+
+idempot :: IO ()
+idempot = hspec $ do
+ describe "capitalise word is idempotent" $ do
+   it "keep on capitalising" $ do
+     property $ \x -> twice capitalizeWord x == fourTimes capitalizeWord x
