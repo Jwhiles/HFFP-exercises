f4526466a3ccae22a4131e7196ae92b1796f4290
John Whiles <john.whiles@justgiving.com>
2017-09-22 15:00:19 +0100

    fix mappend instance

--- exercises/ch15/optional.hs
+++ exercises/ch15/optional.hs
@@ -7,6 +7,7 @@ data Optional a =
 
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
+  mappend (Only a) (Only b) = (Only (a <> b))
   mappend (Only a) _ = (Only a)
   mappend _ (Only a) = (Only a)
   mappend Nada Nada = Nada
