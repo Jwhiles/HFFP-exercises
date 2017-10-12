6baa9060d1ab85f547a21716c991970e4a1be4c8
John Whiles <john.whiles@justgiving.com>
2017-10-12 09:22:12 +0100

    write some monoid instances :)

--- exercises/ch15/chapterexercises/semi.hs
+++ exercises/ch15/chapterexercises/semi.hs
@@ -1,17 +1,28 @@
 import Test.QuickCheck
 import Data.Semigroup
 
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = mappend mempty a == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = mappend a mempty == a
+
 -- 1
 data Trivial = Trivial deriving (Eq, Show)
 
 instance Semigroup Trivial where
   _ <> _ = Trivial
 
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
 instance Arbitrary Trivial where
   arbitrary = return Trivial
 
-semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
-semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
 type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
 
@@ -20,6 +31,10 @@ newtype Identity a = Identity a deriving(Show, Eq)
 
 instance Semigroup a => Semigroup (Identity a) where
   (Identity x) <> (Identity y) = Identity (x <> y)
+
+instance (Semigroup a, Monoid a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
 
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = do
@@ -123,7 +138,11 @@ type OrAssoc a b = Or a b -> Or a b -> Or a b -> Bool
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
   quickCheck (semigroupAssoc :: IdentityAssoc String)
+  quickCheck (monoidLeftIdentity :: Identity String -> Bool)
+  quickCheck (monoidRightIdentity ::  Identity String -> Bool)
   quickCheck (semigroupAssoc :: TwoAssoc String [Int])
   quickCheck (semigroupAssoc :: ThreeAssoc String [Int] String)
   quickCheck (semigroupAssoc :: FourAssoc String [Int] String [Int])
