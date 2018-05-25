a0d0122bdd9f7b1955cdcac417ee20d75bf10629
John Whiles <john.whiles@thetrainline.com>
2018-05-25 15:29:35 +0100

    finish stateT exercises

--- exercises/ch26/stateT.hs
+++ exercises/ch26/stateT.hs
@@ -6,11 +6,23 @@ newtype StateT s m a = StateT { runStateT :: s -> m (a, s) }
 
 instance Functor m => Functor (StateT s m) where
   -- fmap f (StateT smas) = StateT $ \s -> (\(a, s') -> (f a, s')) <$> smas s
-  fmap f (StateT smas) = StateT $ \s -> (over _1 f) <$> smas s
+  fmap f (StateT smas) = StateT $ \s -> (_1 %~ f) <$> smas s
 
-instance Applicative m => Applicative (StateT s m) where
+instance Monad m => Applicative (StateT s m) where
   pure a = StateT $ \s -> (pure (a, s))
+  (<*>) (StateT smf) (StateT sma) = StateT $ \s -> do
+                          (f, s') <- smf s
+                          (_1 %~ f) <$> sma s'
 
-  -- ReaderT fmab <*> ReaderT rma = ReaderT $ fmap (<*>) fmab <*> rma
-  -- StateT smf <*> StateT sma = StateT $ \s -> 
   
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+
+  
+  StateT smas >>= f = StateT $ \s -> do
+                (a, s') <- smas s
+                runStateT (f a) s'
+
+
+                
